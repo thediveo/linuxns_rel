e19110339926b07bb5be6538eee16bdff0315f3a
Harald Albrecht <hidden@hidden.org>
2018-08-04 19:12:01 +0200

    set dev version

--- linuxns_rel/__init__.py
+++ linuxns_rel/__init__.py
@@ -134,7 +134,7 @@ from typing import TextIO
 
 
 # library/package semantic version
-__version__ = '1.0.2webview'
+__version__ = '1.0.2dev'
 
 # Linux namespace type constants; these are used with several of the
 # namespace related functions, such as clone() in particular, but also

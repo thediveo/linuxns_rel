7ca68b11259f136b74766050ecbfe200aafb58f0
Harald Albrecht <hidden@hidden.org>
2019-12-21 19:39:17 +0100

    migrates to pytest fixtures

--- tests/test_nsrel_sandbox.py
+++ tests/test_nsrel_sandbox.py
@@ -17,74 +17,59 @@
 
 import subprocess
 import time
-from typing import Optional
 import pytest
 
 import linuxns_rel as nsr
 from tests.linuxnsrel import LxNsRelationsTestHelper
 
+@pytest.fixture(scope='class')
+def sandbox(request):
+    """Sets up a "sandbox" consisting of an infinite sleep in its own user
+    and network namespaces."""
+    # Oh, the joy of sysctl -w kernel.unprivileged_userns_clone=1
+    request.cls.sandbox = subprocess.Popen([
+        'unshare',  # note that we don't fork!
+        '--user',  # create new user namespace
+        '--map-root-user',
+        '--net',
+        '/bin/sleep', 'infinity'
+    ], close_fds=True)
+    # Now this is ugly ... unshare will need some time to do its dirty work.
+    # If we just speed ahead then some tests may fail later, because the
+    # sandboxed sleeping cat hasn't yet been executed and we still see the
+    # unshare process instead. So, wait and see for our sleeping cat to
+    # settle: only then we'll see its user namespace change.
+    userns_id = request.cls.nsid(request.cls.nspath('user'))
+    while userns_id == request.cls.nsid(
+            request.cls.nspath('user', request.cls.sandbox.pid)):
+        if request.cls.sandbox.poll():
+            print('Skipping sandbox tests, is sysctl '
+                  'kernel.unprivileged_userns_clone disabled?')
+            request.cls.sandbox = None
+            break
+        time.sleep(0.1)
+    yield
+    # Tears down the sandbox.
+    if request.cls.sandbox:
+        request.cls.sandbox.terminate()
+        request.cls.sandbox.wait()
 
-# pylint: disable=invalid-name
-def skipWithoutSandbox(func):  # pragma: no cover
-    """Decorates test functions to be skipped automatically if the
-    unshare sandbox could not be created (due to insufficient
-    privileges or kernel.unprivileged_userns_clone=0)."""
-    def wrapper(self, *args, **kwargs):
-        if getattr(self, 'sandbox', None):
-            func(self, *args, **kwargs)
-        else:
-            pytest.skip('no user unshare sandbox available')
-    return wrapper
+@pytest.fixture(scope='function')
+def sandboxed(request, sandbox): # pylint: disable=redefined-outer-name,unused-argument
+    """Runs an individual test with a separate sandbox, if available; otherwise,
+    skips the individual test."""
+    if not getattr(request.cls, 'sandbox', None):
+        pytest.skip('no user unshare sandbox available')
 
 
+@pytest.mark.usefixtures('sandboxed')
 class TestNsrWithSandbox(LxNsRelationsTestHelper):
-
-    # Only a single sandbox for all tests.
-    sandbox = None  # type: Optional[subprocess.Popen]
-
-    @classmethod
-    def setUpClass(cls): # pragma: no cover
-        """Sets up a "sandbox" consisting of a sleeping cat in its
-        own user namespace. No cats are being harmed during these
-        tests. Only at the end, but don't tell anyone."""
-        # Oh, the joy of sysctl -w kernel.unprivileged_userns_clone=1
-        cls.sandbox = subprocess.Popen([
-            'unshare',  # note that we don't fork!
-            '--user',  # create new user namespace
-            '--map-root-user',
-            '--net',
-            '/bin/cat'
-        ], close_fds=True)
-        # Now this is ugly ... unshare will need some time to do its
-        # dirty work. If we just speed ahead then some tests may fail
-        # later, because the sandboxed sleeping cat hasn't yet been
-        # executed and we still see the unshare process instead. So,
-        # wait and see for our sleeping cat to settle: only then we'll
-        # see its user namespace change.
-        userns_id = cls.nsid(cls.nspath('user'))
-        while userns_id == cls.nsid(
-                cls.nspath('user', cls.sandbox.pid)):
-            if cls.sandbox.poll():
-                print('Skipping sandbox tests, is sysctl '
-                      'kernel.unprivileged_userns_clone disabled?')
-                cls.sandbox = None
-                break
-            time.sleep(0.1)
-
-    @classmethod
-    def tearDownClass(cls): # pragma: no cover
-        """Tears down the sandbox with the sleeping cat. It might be
-        Schrödinger's Cat. Or it might not."""
-        if cls.sandbox:
-            cls.sandbox.terminate()
-            cls.sandbox.wait()
 
     @property
     def sandbox_pid(self):
         """Returns the PID of the sandbox, which is the sleeping cat."""
-        return self.sandbox.pid
+        return type(self).sandbox.pid # type: ignore # pylint: disable=no-member
 
-    @skipWithoutSandbox
     def test_get_user_sandbox(self):
         userns_id = self.nsid(self.nspath('user', self.sandbox_pid))
         with nsr.get_userns(self.nspath('net', self.sandbox_pid)) \
@@ -92,7 +77,6 @@ class TestNsrWithSandbox(LxNsRelationsTestHelper):
             netns_userns_id = self.file_nsid(userns_f)
         assert netns_userns_id == userns_id, 'get_userns returning sandbox user namespace'
 
-    @skipWithoutSandbox
     def test_get_parent_user(self):
         root_userns_id = self.nsid(self.nspath('user'))
         with nsr.get_userns(self.nspath('net', self.sandbox_pid)) \
@@ -101,7 +85,6 @@ class TestNsrWithSandbox(LxNsRelationsTestHelper):
                 parent_userns_id = self.file_nsid(parent_userns_f)
         assert root_userns_id == parent_userns_id, 'get_parentns returning root user namespace'
 
-    @skipWithoutSandbox
     def test_get_user_of_user(self):
         root_userns_id = self.nsid(self.nspath('user'))
         with nsr.get_userns(self.nspath('user', self.sandbox_pid)) \

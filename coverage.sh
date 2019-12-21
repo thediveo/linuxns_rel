9ed35d08bd1c04100df7b343218d38664273ae53
Harald Albrecht <hidden@hidden.org>
2019-12-21 19:56:23 +0100

    adapts coverage (also to vsc)

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,6 @@
 # Runs a unittest coverage and opens the results in the user's default
 # browser. We don't exclude the tests themselves from coverage, so we
 # can go completely meta and check that our tests are covered.
-coverage run -m unittest discover
+pytest --cov-report "xml:cov.xml" --cov linuxns_rel
 coverage html
 xdg-open htmlcov/index.html
